5914081621285a9e59481d30da78f48989a06728
22060 <108120783+22060@users.noreply.github.com>
2024-10-14 15:31:42 +0900

    aaa

---
+++ test.php
@@ -0,0 +1,46 @@
+<?php
+// 新しい単語の追加
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $type = $_POST['type'];
+    $word = $_POST['word'];
+    $mean = $_POST['mean'];
+    $id = $_POST['id'];
+    $userid = $_POST['userid'];
+
+    $url = 'https://github-test.ryutokojou.workers.dev/';
+
+    $post_fields = array(
+        "type" => $type,
+        "word"=> $word,
+        "mean"=> $mean,
+        "id"=> $id,
+        "userid"=> $userid,
+        "token"=> $_COOKIE["user_id"]
+    );
+    $headers = array(
+        "Authorization: aaa",
+        "Content-type: application/json"
+    );
+
+    // curlのセッションを初期化する
+    $ch = curl_init();
+
+    // curlのオプションを設定する
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => json_encode($post_fields)
+    );
+    curl_setopt_array($ch, $options);
+
+    // curlを実行し、レスポンスデータを保存する
+    $response = curl_exec($ch);
+    echo $response;
+    // curlセッションを終了する
+    curl_close($ch);
+
+
+}
+?>
